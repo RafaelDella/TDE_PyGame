a9aa30842cca240290bdb7ab251c17e196d7b231
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-24 18:07:21 -0300

    brincadeira

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+'''Libraries'''
 import pygame
+import sys
+import random
+from math import *
 
+'''Setup Window'''
 pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGTH = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Shooting At Theme Park')
+clock = pygame.time.Clock()
 
 run = True
 while run:
+    mouse_position = pygame.mouse.get_pos()
+    pygame.draw.circle
+
+    if pygame.mouse.get_pressed()[0] == True:
+        print("Left Mouse Clicked")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             run = False
 
-pygame.quit()
+    pygame.display.flip()
+    clock.tick(60)
